2ca7ca974257c6f1f3bb5cb01a96c87b78dab1dd
Kavya chaurasia <118672702+kavyac31@users.noreply.github.com>
2025-04-18 18:24:04 +0530

    Update App.java

--- sbfs-38-value-annotation/SpringExample/src/main/java/car/example/bean/App.java
+++ sbfs-38-value-annotation/SpringExample/src/main/java/car/example/bean/App.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ApplicationContext context
-                = new ClassPathXmlApplicationContext("applicationBeanContext.xml");
+                = new ClassPathXmlApplicationContext("applicationBeanContext.xml"); // xml configuration 
 
         MyBean myBean = (MyBean) context.getBean("myBean");
         System.out.println(myBean);
